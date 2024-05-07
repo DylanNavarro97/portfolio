412399085c3c5a9c553a40f2146b89f5f564151e
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2024-05-07 11:57:44 -0300

    correccion de componentes, reemplace la funcion de navLinks y toggle con Componentes importados.

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import navLogo from "../../assets/icon.png";
 import MoonSVG from "../svg/MoonSVG";
 import SunSVG from "../svg/SunSVG";
+import NavLinks from "./NavLinks";
+import Toggle from "../svg/Toggle";
 
 const Header = ({ darkMode, changeTheme }) => {
   const [isOpen, setisOpen] = useState(false);
@@ -17,66 +19,6 @@ const Header = ({ darkMode, changeTheme }) => {
     setisOpen(!isOpen);
   };
 
-  const toggle = () => {
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="42"
-        height="42"
-        fill="currentColor"
-        className="bi bi-list"
-        viewBox="0 0 16 16"
-      >
-        <path
-          fillRule="evenodd"
-          d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
-        />
-      </svg>
-    );
-  };
-
-  const navLinks = () => {
-    return (
-      <>
-        <a
-          href="#"
-          onClick={toggleNavbar}
-          className="hover:text-fuchsia-500 text-xl"
-        >
-          Inicio
-        </a>
-        <a
-          href="#"
-          onClick={toggleNavbar}
-          className="hover:text-fuchsia-500 text-xl"
-        >
-          Sobre mi
-        </a>
-        <a
-          href="#"
-          onClick={toggleNavbar}
-          className="hover:text-fuchsia-500 text-xl"
-        >
-          Experiencia
-        </a>
-        <a
-          href="#"
-          onClick={toggleNavbar}
-          className="hover:text-fuchsia-500 text-xl"
-        >
-          Habilidades
-        </a>
-        <a
-          href="#"
-          onClick={toggleNavbar}
-          className="hover:text-fuchsia-500 text-xl"
-        >
-          Contacto
-        </a>
-      </>
-    );
-  };
-
   return (
     <header className={darkMode ? "dark" : "light"}>
       <nav className={`flex justify-between px-4 md:px-12 brandNameContainer ${darkMode && "border-neutral-100"}`}>
@@ -88,19 +30,19 @@ const Header = ({ darkMode, changeTheme }) => {
         </div>
 
         <div className="hidden md:flex justify-between items-center gap-3">
-          {navLinks()}
+          <NavLinks toggleNavbar={toggleNavbar}/>
           {darkMode ? <button onClick={changeTheme}><MoonSVG/></button> : <button onClick={changeTheme}><SunSVG /></button>}
         </div>
 
         <div className="md:hidden flex items-center gap-3">
           {darkMode ? <button onClick={changeTheme}><MoonSVG/></button> : <button onClick={changeTheme}><SunSVG /></button>}
-          <button onClick={toggleNavbar}>{toggle()}</button>
+          <button onClick={toggleNavbar}> <Toggle /> </button>
         </div>
       </nav>
       
       <div className={`md:hidden navMenuMob overflow-hidden ${isOpen ? "navMenuMobOpen" : "navMenuMobClosed"}`} >
         <div className="flex flex-col items-center py-4 gap-3 border-t-2 border-t-gray-500">
-          {navLinks()}
+          <NavLinks toggleNavbar={toggleNavbar}/>
         </div>
       </div>
     </header>
---
+++ src/components/common/NavLinks.jsx
@@ -0,0 +1,46 @@
+import { useEffect } from "react";
+
+const NavLinks = ({ toggleNavbar }) => {
+
+  return (
+    <>
+      <a
+        href="#"
+        onClick={toggleNavbar}
+        className="hover:text-fuchsia-500 text-xl"
+      >
+        Inicio
+      </a>
+      <a
+        href="#"
+        onClick={toggleNavbar}
+        className="hover:text-fuchsia-500 text-xl"
+      >
+        Sobre mi
+      </a>
+      <a
+        href="#"
+        onClick={toggleNavbar}
+        className="hover:text-fuchsia-500 text-xl"
+      >
+        Experiencia
+      </a>
+      <a
+        href="#"
+        onClick={toggleNavbar}
+        className="hover:text-fuchsia-500 text-xl"
+      >
+        Habilidades
+      </a>
+      <a
+        href="#"
+        onClick={toggleNavbar}
+        className="hover:text-fuchsia-500 text-xl"
+      >
+        Contacto
+      </a>
+    </>
+  );
+};
+
+export default NavLinks;
